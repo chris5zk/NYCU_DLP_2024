b6e058d4bee35e544cdb30ad1ee5021c7102f705
chris5zk <wzk789wzk@gmail.com>
2024-04-09 02:04:58 +0800

    unet update-21: data transform

--- lab3/src/utils.py
+++ lab3/src/utils.py
@@ -8,7 +8,6 @@ class DICEloss(nn.Module):
         self.eps = 1e-5
         
     def forward(self, pred, target):
-        print(pred, target)
         intersection = 2 * torch.sum(pred * target) + self.eps
         union = torch.sum(pred) + torch.sum(target) + self.eps
         loss = 1 - intersection / union
